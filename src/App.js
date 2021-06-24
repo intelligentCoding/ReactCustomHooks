0181276e6afd958137846abcdb3db5198b2ecb68
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-06-24 12:29:23 -0600

    custom hook implemented

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
-
+import useHttp from './hooks/use-http';
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
+  const transformTasks  = useCallback((taskObj) => {
+    console.log(taskObj)
+    const loadedTasks = [];
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        `${process.env.REACT_APP_FIREBASE_URL}/task.json`
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in taskObj) {
+      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
     }
-    setIsLoading(false);
-  };
+
+    setTasks(loadedTasks);
+  }, [])
+  const {isLoading, error, sendRequest: fetchTasks} = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks({url: `${process.env.REACT_APP_FIREBASE_URL}/task.json`}, transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
