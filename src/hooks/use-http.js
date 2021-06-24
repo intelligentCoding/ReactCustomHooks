7652747088d6f2259e881b29668d81e4c9068329
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-06-24 12:52:13 -0600

    code clean

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,39 +1,39 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useState } from "react";
 
 const useHttp = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-  
-    const sendRequest = useCallback(async (requestConfig, applyData) => {
-        setIsLoading(true);
-        setError(null);
-        try {
-            const response = await fetch(requestConfig.url,{
-                    method: requestConfig.method ? requestConfig.method : 'GET' ,
-                    headers: requestConfig.headers ? requestConfig.headers : {},
-                    body: requestConfig.body ? JSON.stringify(requestConfig.body) : null
-                  }
-                  );
-    
-          if (!response.ok) {
-            throw new Error('Request failed!');
-          }
-    
-          console.log(response)
-          const data = await response.json();
-          applyData(data)
-    
-        } catch (err) {
-          setError(err.message || 'Something went wrong!');
-        }
-        setIsLoading(false);
-      }, [])
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    return {
-        isLoading,
-        error,
-        sendRequest
-    };
-}
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      //If request failed
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+
+      //call the function in the component and send data.
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
 
 export default useHttp;
